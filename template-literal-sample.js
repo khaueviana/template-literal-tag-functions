adb9caa5df831915330c08d87d6fb7ec62b989ba
Khauê Viana <khaueviana@gmail.com>
2017-05-27 17:02:56 -0300

    add template literal sample

---
+++ template-literal-sample.js
@@ -0,0 +1,30 @@
+var p = {
+    name: 'Jackson',
+    nn: 'Jak'
+}
+
+//String Concatenation
+console.log('Hi, I\'m ' + p.name + '! Call me "' + p.nn + '".');
+
+//Template Literals
+console.log(`Hi, I'm ${p.name}! Call me "${p.nn}".`);
+
+// STRING CONCATENATION
+console.log("Dear Mom,\n" + 
+"Hope you are well.\n" + 
+"\tLove, your son")
+
+// TEMPLATE LITERALS
+console.log(`Dear Mom,
+Hope you are well.
+    Love, your son`);
+
+// Dear Mom,
+// Hope you are well.
+//     Love, your son
+
+// STRING CONCATENATION
+console.log('Three plus six is ' + (3 + 6) + '.');
+// TEMPLATE LITERALS
+console.log(`Three plus six is ${3 + 6}.`);
+// "Three plus six is 9."
